505095ff65ae504e4ec4820da92d6eb9230a2c6f
Dayan <dayanjayasuriya7@gmail.com>
2019-11-26 16:44:59 -0700

    Added firebase

---
+++ app/src/main/java/com/example/rello/onboarding_2_activity.kt
@@ -0,0 +1,61 @@
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc
+	 *	@file 		start_up_screen
+	 *	@date 		0
+	 *	@title 		Start-up Screen
+	 *	@author
+	 *	@keywords
+	 *	@generator 	Export Kit v1.2.9.xd
+	 *
+	 */
+
+
+package com.example.rello
+
+import android.app.Activity
+import android.os.Bundle
+
+
+import android.view.View
+import android.widget.TextView
+import android.widget.ImageView
+
+class onboarding_2_activity : Activity() {
+
+
+    private var _bg__onboarding_2_ek2: View? = null
+    private var groups: TextView? = null
+    private var find_all_of_your_groups__you_can_also_join_other_groups_and_create_your_own_groups_under_this_tab_: TextView? =
+        null
+    private var bitmap_ek4: ImageView? = null
+    private var ellipse_8_ek1: View? = null
+    private var ellipse_9_ek1: View? = null
+    private var ellipse_10_ek1: View? = null
+    private var ellipse_11_ek1: View? = null
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.onboarding_2)
+
+
+        _bg__onboarding_2_ek2 = findViewById(R.id._bg__onboarding_2_ek2) as View
+        groups = findViewById<View>(R.id.groups) as TextView
+        find_all_of_your_groups__you_can_also_join_other_groups_and_create_your_own_groups_under_this_tab_ =
+            findViewById<View>(R.id.find_all_of_your_groups__you_can_also_join_other_groups_and_create_your_own_groups_under_this_tab_) as TextView
+        bitmap_ek4 = findViewById<View>(R.id.bitmap_ek4) as ImageView
+        ellipse_8_ek1 = findViewById(R.id.ellipse_8_ek1) as View
+        ellipse_9_ek1 = findViewById(R.id.ellipse_9_ek1) as View
+        ellipse_10_ek1 = findViewById(R.id.ellipse_10_ek1) as View
+        ellipse_11_ek1 = findViewById(R.id.ellipse_11_ek1) as View
+
+
+        //custom code goes here
+
+    }
+}
+	
+	
